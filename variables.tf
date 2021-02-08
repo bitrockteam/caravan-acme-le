69e6baa3318db787cbe9a3421bc54253427e3960
Simone Ripamonti <simone.ripamonti@bitrock.it>
2021-02-08 11:49:37 +0100

    Add azure support (#4)

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "private_key" {
 variable "dns_provider" {
   type = string
   validation {
-    condition     = contains(["aws", "gcp"], var.dns_provider)
+    condition     = contains(["aws", "gcp", "azure"], var.dns_provider)
     error_message = "Unsupported dns_provider."
   }
 }
@@ -55,4 +55,22 @@ variable "aws_zone_id" {
   type        = string
   default     = ""
   description = "When using dns_provider==aws, the AWS zone id"
+}
+
+// Azure
+variable "azure_tenant_id" {
+  type    = string
+  default = ""
+}
+variable "azure_resource_group" {
+  type    = string
+  default = ""
+}
+variable "azure_client_id" {
+  type    = string
+  default = ""
+}
+variable "azure_client_secret" {
+  type    = string
+  default = ""
 }
