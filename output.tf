9afb960114d60bc126de13af1898a9ed73f31262
Simone Ripamonti <simone.ripamonti@bitrock.it>
2021-02-09 15:27:43 +0100

    Add PFX certificate

--- output.tf
+++ output.tf
@@ -9,3 +9,11 @@ output "certificate_pem" {
 output "private_key_pem" {
   value = acme_certificate.certificate.private_key_pem
 }
+
+output "certificate_p12" {
+  value = acme_certificate.certificate.certificate_p12
+}
+
+output "certificate_p12_password" {
+  value = acme_certificate.certificate.certificate_p12_password
+}
