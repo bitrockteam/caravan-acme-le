20bf430b54cb73ee8241b126ace4a70941e3323f
Simone Ripamonti <simone.ripamonti@bitrock.it>
2021-02-11 13:02:42 +0100

    Add tls key autogen for p12 (#6)

--- output.tf
+++ output.tf
@@ -1,19 +1,19 @@
 output "issuer_pem" {
-  value = acme_certificate.certificate.issuer_pem
+  value = local.private_key_provided ? acme_certificate.certificate_from_csr[0].issuer_pem : acme_certificate.certificate_auto[0].issuer_pem
 }
 
 output "certificate_pem" {
-  value = acme_certificate.certificate.certificate_pem
+  value = local.private_key_provided ? acme_certificate.certificate_from_csr[0].certificate_pem : acme_certificate.certificate_auto[0].certificate_pem
 }
 
 output "private_key_pem" {
-  value = acme_certificate.certificate.private_key_pem
+  value = local.private_key_provided ? var.private_key : acme_certificate.certificate_auto[0].private_key_pem
 }
 
 output "certificate_p12" {
-  value = acme_certificate.certificate.certificate_p12
+  value = local.private_key_provided ? acme_certificate.certificate_from_csr[0].certificate_p12 : acme_certificate.certificate_auto[0].certificate_p12
 }
 
 output "certificate_p12_password" {
-  value = acme_certificate.certificate.certificate_p12_password
+  value = local.private_key_provided ? acme_certificate.certificate_from_csr[0].certificate_p12_password : acme_certificate.certificate_auto[0].certificate_p12_password
 }
